7efb481a36493eaa4f37eb1477210dd1de340f95
HghaVlad <vladbax6@gmail.com>
2023-03-08 22:31:43 +0300

    Delete files from another branch

--- example_sites/task1/dictionary/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-   path("", views.home_page),
-   path("home", views.home_page),
-   path("add_word", views.add_word),
-   path("words_list", views.words_list)
-]
